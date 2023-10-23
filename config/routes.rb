55428aea878f9e905b1ceafd6ed6128e16bdbb0f
EC2 Default User <ec2-user@ip-172-31-37-167.ap-northeast-1.compute.internal>
2023-10-23 08:33:14 +0000

    [Finish]bookers2-debug4

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  get 'searches/search'
   get 'relationships/create'
   get 'relationships/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
   root to: "homes#top"
   get "home/about"=>"homes#about"
+  get "search" => "searches#search"
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update]do
     resources :book_comments, only: [:create,:destroy]
@@ -16,6 +18,7 @@ Rails.application.routes.draw do
     end
       resource :relationships, only: [:create, :destroy]
   end
+  
   
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
